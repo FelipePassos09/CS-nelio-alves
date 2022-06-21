e1d9707824360c86bebdf4fe55a74588ff9da398
felipepassos09 <felipe.passos.09@gmail.com>
2022-06-21 19:49:35 -0300

    Update

--- Projetos/Testes(Console)/Rider/PrimeiroProjeto/PrimeiroProjeto/Main.cs
+++ Projetos/Testes(Console)/Rider/PrimeiroProjeto/PrimeiroProjeto/Main.cs
@@ -5,7 +5,16 @@ public class Main
 {
     public static void Print(string[] args)
     {
+        Collection<nomes> = ("João", "Felipe", "Pedro", "Maria", "Janice", "Mari");
+        
         Console.WriteLine("Olá Mundo");
+
+        foreach (var nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+
+        
     }
     
 }
