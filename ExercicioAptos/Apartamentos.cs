bd359dd662eedfd7c1aa3718feb645e32c496866
felipe <felipe.passos.09@gmail.com>
2022-08-11 21:25:19 -0300

    Exercicio de Fixação

---
+++ ExercicioAptos/Apartamentos.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace ExercicioAptos;
+
+public class Apartamento
+{
+    private string Inquilino { set; get; }
+    private string Email { set; get; }
+    private int Apto { set; get; }
+    private double Aluguel { set; get; }
+
+    public Apartamento(string name, string email, int apto, double price)
+    {
+        Inquilino = name;
+        Email = email;
+        Apto = apto;
+        Aluguel = price;
+    }
+
+    public double TotalCash(Apartamento[] vetor)
+    {
+        double total = 0.00;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            total += Aluguel;
+        }
+
+        return total;
+    }
+    
+    public override string ToString()
+    {
+        return $"Nome: {Inquilino}\nE-mail: {Email}\nApartamento: {Apto}\nAluguel: {Aluguel.ToString("F2", CultureInfo.InvariantCulture)}";
+    }
+}
---
+++ ExercicioAptos/Main.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+#pragma warning disable CS8600
+#pragma warning disable CS8604
+
+namespace ExercicioAptos;
+
+public class main
+{
+    static void Main()
+    {
+        Console.WriteLine("Programa de acompanhamento de quartos");
+        Apartamento[] list = new Apartamento[3];
+
+        Console.WriteLine("Vamos começar com os cadastros:");
+        for (int i = 0; i < list.Length; i++)
+        {
+            Console.Write($"Insira o nome do {i+1}º inquilino: ");
+            string inq = Console.ReadLine();
+            Console.Write("Insira, agora, o e-mail do inquilino: ");
+            string email = Console.ReadLine();
+            Console.Write("Agora insira o número do apartamento: ");
+            int apto = int.Parse(Console.ReadLine());
+            Console.Write("Por fim, insira o valor a pagar de aluguel: ");
+            double alg = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\n");
+                
+            list[apto-1] = new Apartamento(inq, email, apto, alg);
+        }
+
+        for (int i = 0; i < list.Length; i++)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine(list[i]);
+        }
+
+        Console.WriteLine($"O valor total, mensal, recebido será: {list[0].TotalCash(list).ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
